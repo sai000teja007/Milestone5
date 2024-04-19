8cd36aae87158a353eaf5215f513eb32f97507da
Sai Teja Thatikonda <137807543+sai000teja007@users.noreply.github.com>
2024-04-19 17:20:39 -0500

    Delete TrainerRepository.java

--- TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tejaClasses.DMS.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tejaClasses.DMS.Models.Trainer;
-
-public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
-
-}
